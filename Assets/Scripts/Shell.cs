3f6413d134601d3fba591c2f221e0263da394eb9
jofto <slammerex03@gmail.com>
2023-10-24 12:48:24 +0100

    3.Calculating Trajectories Part 3

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -2,42 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shell : MonoBehaviour
-{
-    float speed = 0f;
-    float yspeed = 0f;
-    float mass = 10;
-    float force = 2;
-    float drag = 1;
+public class Shell : MonoBehaviour {
+
+    public GameObject explosion;
+    float speed = 0.0f;
+    float ySpeed = 0.0f;
+    float mass = 30.0f;
+    float force = 4.0f;
+    float drag = 1.0f;
     float gravity = -9.8f;
     float gAccel;
     float acceleration;
 
-    public GameObject explosion;
 
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.tag == "tank")
-        {
+    void OnCollisionEnter(Collision col) {
+
+        if (col.gameObject.tag == "tank") {
             GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
             Destroy(exp, 0.5f);
             Destroy(this.gameObject);
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    private void Start() {
+
         acceleration = force / mass;
-        speed += acceleration * 1;
+        speed += acceleration * 1.0f;
         gAccel = gravity / mass;
     }
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
+    void LateUpdate() {
+
         speed *= (1 - Time.deltaTime * drag);
-        yspeed += gAccel * Time.deltaTime;
-        this.transform.Translate(0, yspeed, speed);
+        ySpeed += gAccel * Time.deltaTime;
+        this.transform.Translate(0.0f, ySpeed, speed);
     }
 }

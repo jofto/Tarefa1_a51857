15f720ddd98bb7f277b14006803e0e28f9e1e9e6
jofto <slammerex03@gmail.com>
2023-11-08 23:45:56 +0000

    2.Fleeing Part 2

--- Assets/AIControl.cs
+++ Assets/AIControl.cs
@@ -8,6 +8,9 @@ public class AIControl : MonoBehaviour {
     GameObject[] goalLocations;
     NavMeshAgent agent;
     Animator anim;
+    float speedMult;
+    float detectionRadius = 20;
+    float fleeRadius = 10;
 
     void Start() {
 
@@ -18,15 +21,44 @@ public class AIControl : MonoBehaviour {
         anim = this.GetComponent<Animator>();
         anim.SetTrigger("isWalking");
         anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
-        float sm = Random.Range(0.5f, 2.0f);
-        anim.SetFloat("speedMult", sm);
-        agent.speed *= sm;
+        ResetAgent();
+    }
+
+    void ResetAgent()
+    {
+        speedMult = Random.Range(0.1f, 1.5f);
+        anim.SetFloat("speedMult", speedMult);
+        agent.speed *= speedMult;
+        anim.SetTrigger("isWalking");
+        agent.ResetPath();
+        agent.angularSpeed = 120;
+    }
+
+    public void DetectNewObstacle(Vector3 position)
+    {
+        if (Vector3.Distance(position, this.transform.position) < detectionRadius)
+        {
+            Vector3 fleeDirection = (this.transform.position - position).normalized;
+            Vector3 newgoal = this.transform.position + fleeDirection * fleeRadius;
+
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(newgoal, path);
+
+            if (path.status != NavMeshPathStatus.PathInvalid)
+            {
+                agent.SetDestination(path.corners[path.corners.Length - 1]);
+                anim.SetTrigger("isRunning");
+                agent.speed = 10;
+                agent.angularSpeed = 500;
+            }
+        }
     }
 
 
     void Update() {
         if (agent.remainingDistance < 1)
         {
+            ResetAgent();
             int i = Random.Range(0, goalLocations.Length);
             agent.SetDestination(goalLocations[i].transform.position);
         }

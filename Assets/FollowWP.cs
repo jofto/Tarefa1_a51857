6b1496ba16293e7cdfbf4684a780455083e435d4
jofto <slammerex03@gmail.com>
2023-11-10 20:51:41 +0000

    2.Slerping to the Direction of Travel

--- Assets/FollowWP.cs
+++ Assets/FollowWP.cs
@@ -8,6 +8,7 @@ public class FollowWP : MonoBehaviour
     int currentWP = 0;
 
     public float speed = 10.0f;
+    public float rotSpeed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +19,18 @@ public class FollowWP : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 10)
             currentWP++;
 
         if(currentWP >= waypoints.Length)
             currentWP = 0;
 
-        this.transform.LookAt(waypoints[currentWP].transform);
+        //this.transform.LookAt(waypoints[currentWP].transform);
+
+        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+
         this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
